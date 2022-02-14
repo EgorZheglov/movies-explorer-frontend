4130f7db2b8833541cb00fdda73f82bbdf7e4d15
Egor <egorzh97@yandex.ru>
2022-02-14 20:40:07 +0300

    revert

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,42 +2,20 @@ import { Link } from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
 import "./Promo.css";
 
-function Promo(props) {
+function Promo() {
   return (
     <div className="promo">
-      {!props.loggedIn ? (
-        <div className="promo__header">
-          <div className="promo__logo" />
-          <div className="promo__buttons">
-            <Link to="/sign-up" className="promo__signup-button link">
-              Регистрация
-            </Link>
-            <Link to="/login" className="promo__login-button link">
-              Войти
-            </Link>
-          </div>
+      <div className="promo__header">
+        <div className="promo__logo" />
+        <div className="promo__buttons">
+          <Link to="/sign-up" className="promo__signup-button link">
+            Регистрация
+          </Link>
+          <Link to="/login" className="promo__login-button link">
+            Войти
+          </Link>
         </div>
-      ) : (
-        <div className="promo__header">
-          <Link to="/" className="promo__logo" />
-          <div className="promo__buttons">
-            <Link to="/movies" className="promo__nav-button link">
-              Фильмы
-            </Link>
-            <Link to="/saved-movies" className="promo__nav-button link">
-              Сохранённые фильмы
-            </Link>
-            <Link
-              to="/profile"
-              className="promo__nav-button promo__nav-button_profile link"
-            >
-              Аккаунт
-              <div className="promo__acc-icon"></div>
-            </Link>
-            <Link to="/menu" className="promo__menu-button link" />
-          </div>
-        </div>
-      )}
+      </div>
       <div className="promo__block">
         <div className="promo__title">
           Учебный проект студента факультета Веб-разработки.
